f13f491bc2fc25f4ddef64c9fbb17e4ff9af97dc
chukka jayanth <chjayanth119@gmail.com>
2023-12-08 12:24:30 +0530

    1 Adding Splash Screen
    building splash screen to human_genx

---
+++ lib/intro.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+
+class Splash_screen extends StatefulWidget {
+  const Splash_screen({super.key});
+
+  @override
+  State<Splash_screen> createState() => _Splash_screenState();
+}
+
+class _Splash_screenState extends State<Splash_screen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Icon(Icons.ac_unit_rounded);
+  }
+}
